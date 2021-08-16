77f0dc100c39d0325699e8e4204bec53fa797bb6
davinciobandoreyes <85654905+davinciobandoreyes@users.noreply.github.com>
2021-08-16 18:33:35 -0500

    Contact Form Fix Ver 1
    Test

--- forms/contact.php
+++ forms/contact.php
@@ -7,6 +7,22 @@
   */
 
   // Replace contact@example.com with your real receiving email address
+
+
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $recipient = $_POST['davinciobandoreyes@gmail.com'];
+
+  $mailheader = "From" .$name. "<" .$email. "> \r\n";
+
+  mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+  echo "message send";
+
+/*
   $receiving_email_address = 'davinciobandoreyes@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -23,7 +39,10 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
+  */
+
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+
   /*
   $contact->smtp = array(
     'host' => 'example.com',
@@ -33,9 +52,13 @@
   );
   */
 
+/*
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+
+*/
+  
 ?>
